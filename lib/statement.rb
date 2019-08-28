78429af6c4da783efd40e5a2709664bca6300fba
Freddie <f.varnals@gmail.com>
2019-08-28 14:58:29 +0100

    Statement #balance returns a float

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,10 @@
 class Statement
+
   def headers
     'date || credit || debit || balance'
+  end
+
+  def balance
+    1.00
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,7 @@ describe Statement do
   it "should have correct headers" do
     expect(subject.headers).to eq('date || credit || debit || balance')
   end
+  it "should show a float balance with two decimal places" do
+    expect(subject.balance).to be_a(Float)
+  end
 end
