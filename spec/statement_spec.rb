21239b43f41a8f202ac2b08a9e3695ac604bb290
Freddie <f.varnals@gmail.com>
2019-08-31 19:53:46 +0100

    Refactored statement tests for better readability

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,53 +1,54 @@
 require 'statement'
 
 describe Statement do
+  subject(:statement) { described_class.new }
   it "should have correct headers" do
-    expect(subject.headers).to eq('date || credit || debit || balance')
+    expect(statement.headers).to eq('date || credit || debit || balance')
   end
 
   describe '#debit_amount' do
     it "should respond to #debit_amount" do
-      expect(subject).to respond_to(:debit_amount)
+      expect(statement).to respond_to(:debit_amount)
     end
     it "should accept a float as an argument" do
-      expect{subject.debit_amount(1.00)}.not_to raise_error
+      expect{statement.debit_amount(1.00)}.not_to raise_error
     end
     it "should return amount debited as string" do
-      expect(subject.debit_amount(1.0)).to eq("1.00")
+      expect(statement.debit_amount(1.0)).to eq("1.00")
     end
   end
   describe '#credit_amount' do
     it "should respond to #credit" do
-      expect(subject).to respond_to(:credit_amount)
+      expect(statement).to respond_to(:credit_amount)
     end
     it "should accept a float as an argument" do
-      expect{subject.credit_amount(1.00)}.not_to raise_error
+      expect{statement.credit_amount(1.00)}.not_to raise_error
     end
     it "should return amount credited as string" do
-      expect(subject.credit_amount(1.0)).to eq("1.00")
+      expect(statement.credit_amount(1.0)).to eq("1.00")
     end
   end
   describe '#balance_after_transaction' do
     it 'returns the balance as a float string, following transaction' do
-      expect(subject.balance_after_transaction(2)).to eq("2.00")
+      expect(statement.balance_after_transaction(2)).to eq("2.00")
     end
   end
   describe '#transaction_date' do
     it 'returns the date for a transaction' do
-      expect(subject.transaction_date('27/01/2014')).to eq('27/01/2014')
+      expect(statement.transaction_date('27/01/2014')).to eq('27/01/2014')
     end
   end
   describe 'record transactions' do
     it 'stores the history of transactions' do
-      expect(subject).to respond_to(:transaction_history)
+      expect(statement).to respond_to(:transaction_history)
     end
   end
   describe '#print_statement' do
     it 'should return a list of all transactions under specified headers' do
-      subject.debit_amount(1000)
-      subject.debit_amount(2000)
-      subject.credit_amount(500)
-      expect(subject.print_statement).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00")
+      statement.debit_amount(1000)
+      statement.debit_amount(2000)
+      statement.credit_amount(500)
+      expect(statement.print_statement).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00")
     end
   end
 end
