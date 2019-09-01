de3402ded31b7f08dca2d60e549142b6eb641f8a
Freddie <f.varnals@gmail.com>
2019-09-01 16:43:36 +0100

    Transaction shows the resultant balance of the account it is associated with

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction
 
-  attr_reader :date, :type, :amount
+  attr_reader :date, :type, :amount, :account_balance
 
-  def initialize(type, amount = '0.00', date = Date.today.to_s)
+  def initialize(type, account_balance, amount = '0.00', date = Date.today.to_s)
     @type = type
     @amount = amount
     @date = date
+    @account_balance = account_balance
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,21 +1,21 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { described_class.new('test') }
+  subject(:transaction) { described_class.new('test', 100) }
   it "is instance of the Transaction class" do
     expect(transaction).to be_an_instance_of(Transaction)
   end
   it 'has a date associated with it' do
     date_today = Date.today.to_s
     expect(transaction.date).to eq(date_today)
-    p transaction.date
-    p transaction.type
-    p transaction.amount
   end
   it 'has a type i.e. deposit' do
     expect(transaction.type).to eq('test')
   end
   it 'has a monetary value associated with it' do
-    expect(transaction.amount).to eq(0.00)
+    expect(transaction.amount).to eq('0.00')
+  end
+  it 'records the resultant balance of the account it is associated with' do
+    expect(transaction.account_balance).to eq(100.00)
   end
 end
