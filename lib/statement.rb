c6e51bd7c045a61c8fd3e83517045f940068a953
Freddie <f.varnals@gmail.com>
2019-09-01 14:00:42 +0100

    Statement shows transactions in reverse chronological order. NB this required reversing both the order of hash keys, and the order of transactions (array values for each hash key) since these are added to transaction_history in chronological order. Error fixed in #print_statement to ensure that all transaction and balance amounts are shown to 2 decimal places

--- lib/statement.rb
+++ lib/statement.rb
@@ -32,10 +32,12 @@ class Statement
     # "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
     transaction_history = account.transaction_history
     transactions = ""
+    transaction_history = transaction_history.reverse_each.to_h
     transaction_history.each do |date, activities|
+      activities = activities.reverse_each.to_a
       activities.each do |transaction|
         if transaction[0] == "withdrawl"
-          transactions += "\n#{reformat_date(date)} || #{transaction[1]} || || #{transaction[2]}"
+          transactions += "\n#{reformat_date(date)} || #{credit_amount(transaction[1])} || || #{balance_after_transaction(transaction[2])}"
         else transaction[0] == "deposit"
           transactions += "\n#{reformat_date(date)} || || #{debit_amount(transaction[1])} || #{balance_after_transaction(transaction[2])}"
         end
