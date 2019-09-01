4850112ccb2cce3fe45f3ad198e0c043313ec8c5
Freddie <f.varnals@gmail.com>
2019-09-01 20:11:50 +0100

    Refactoring and removing unnecessary code

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,27 +1,21 @@
 class Statement
 
-  attr_reader :balance, :transaction_history
-
-  def initialize(balance=0.00)
-    @balance = balance
-  end
-
   def headers
     'date || credit || debit || balance'
   end
 
   def print_statement(account)
     transaction_history = account.transaction_history
-    formatted_transactions = format_transactions(transaction_history)
-    return "#{headers}" + "#{formatted_transactions}"
+    transactions = format_transactions(transaction_history)
+    return "#{headers}" + "#{transactions}"
   end
 
   def format_transactions(transaction_history)
-    transactions = ""
+    transactions = ''
     transaction_history.reverse_each do |transaction|
-      if transaction.type == "deposit"
+      if transaction.type == 'deposit'
         transactions += "\n#{transaction.date} || #{transaction.amount} || || #{transaction.account_balance}"
-      else transaction.type == "credit"
+      else transaction.type == 'credit'
         transactions += "\n#{transaction.date} || || #{transaction.amount} || #{transaction.account_balance}"
       end
     end
