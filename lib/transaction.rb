4bdba7b2aae0d1916c848d21629bd22d1f9c0a78
Freddie <f.varnals@gmail.com>
2019-09-01 16:37:41 +0100

    Transaction amount changed to string format to allow for cleaner code in later processing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,7 +2,7 @@ class Transaction
 
   attr_reader :date, :type, :amount
 
-  def initialize(type, amount = 0.00, date = Date.today.to_s)
+  def initialize(type, amount = '0.00', date = Date.today.to_s)
     @type = type
     @amount = amount
     @date = date
