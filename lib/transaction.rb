29780aae2978564273b53190fe5de9efccc28d21
Freddie <f.varnals@gmail.com>
2019-09-01 16:24:01 +0100

    Transaction has a date associated with it

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction
+
+  attr_reader :date
+
+  def initialize(date = Date.today.to_s)
+    @date = date
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,4 +5,8 @@ describe Transaction do
   it "is instance of the Transaction class" do
     expect(transaction).to be_an_instance_of(Transaction)
   end
+  it 'has a date associated with it' do
+    transaction = Transaction.new('2019-09-01')
+    expect(transaction.date).to eq('2019-09-01')
+  end
 end
